bfe071065b4b759a213d172db46b4285ab963e97
Matey-Nikolov <matey.nikolov@mail.bg>
2021-09-20 19:27:06 +0300

    Matrix task.

---
+++ Snake_Moves/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Snake_Moves
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] nM = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = nM[0];
+            int m = nM[1];
+
+            char[,] matrix = new char[n, m];
+
+            string snake = Console.ReadLine();
+            int x = -1;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if (x == snake.Length - 1)
+                            x = -1;
+
+                        x++;
+                        matrix[i, j] = snake[x];
+                    }
+                }
+                else
+                {
+                    for (int j = matrix.GetLength(1) - 1; j >= 0; j--)
+                    {
+                        if (x == snake.Length - 1)
+                            x = -1;
+
+                        x++;
+                        matrix[i, j] = snake[x];
+                    }
+                }
+            }
+
+            for (int k = 0; k < matrix.GetLength(0); k++)
+            {
+                for (int l = 0; l < matrix.GetLength(1); l++)
+                {
+                    Console.Write($"{matrix[k, l]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
